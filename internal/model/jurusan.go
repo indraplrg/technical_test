1da1ee5c4105a488128ae62f5fcfec8bb70bf968
indraplrg <indraplrg@gmail.com>
2026-08-07 08:18:49 +0800

    feat(jurusan): create jurusan model

---
+++ internal/model/jurusan.go
@@ -0,0 +1,23 @@
+package model
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+// Jurusan represents an academic department. One Jurusan has many Mahasiswa.
+type Jurusan struct {
+	ID          uint   `gorm:"primaryKey" json:"id_jurusan"`
+	NamaJurusan string `gorm:"size:100;not null" json:"nama_jurusan"`
+	Fakultas    string `gorm:"size:100;not null" json:"fakultas"`
+	Jenjang     string `gorm:"size:50;not null" json:"jenjang"`
+
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
+}
+
+func (Jurusan) TableName() string {
+	return "jurusan"
+}
